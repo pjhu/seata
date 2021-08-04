277615d9cfb7c021598f17590ef0eb47bfde21a7
Jinhu Peng <jhpeng@thoughtworks.com>
2021-08-04 08:54:43 +0800

    setup local consul

--- devops/local/consul/consul-config.sh
+++ devops/local/consul/consul-config.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-while getopts ":h:p:g:t:u:w:" opt
+while getopts ":h:p:" opt
 do
   case $opt in
   h)
@@ -22,83 +22,46 @@ do
   p)
     port=$OPTARG
     ;;
-  g)
-    group=$OPTARG
-    ;;
-  t)
-    tenant=$OPTARG
-    ;;
-  u)
-    username=$OPTARG
-    ;;
-  w)
-    password=$OPTARG
-    ;;
   ?)
-    echo " USAGE OPTION: $0 [-h host] [-p port] [-g group] [-t tenant] [-u username] [-w password] "
+    echo " USAGE OPTION: $0 [-h host] [-p port] "
     exit 1
     ;;
   esac
 done
 
-urlencode() {
-  for ((i=0; i < ${#1}; i++))
-  do
-    char="${1:$i:1}"
-    case $char in
-    [a-zA-Z0-9.~_-]) printf $char ;;
-    *) printf '%%%02X' "'$char" ;;
-    esac
-  done
-}
-
 if [[ -z ${host} ]]; then
     host=localhost
 fi
 if [[ -z ${port} ]]; then
-    port=8848
-fi
-if [[ -z ${group} ]]; then
-    group="SEATA_GROUP"
-fi
-if [[ -z ${tenant} ]]; then
-    tenant=""
-fi
-if [[ -z ${username} ]]; then
-    username=""
-fi
-if [[ -z ${password} ]]; then
-    password=""
+    port=8500
 fi
 
-nacosAddr=$host:$port
+consulAddr=$host:$port
 contentType="content-type:application/json;charset=UTF-8"
-
-echo "set nacosAddr=$nacosAddr"
-echo "set group=$group"
+echo "Set consulAddr=$consulAddr"
 
 failCount=0
 tempLog=$(mktemp -u)
 function addConfig() {
-  curl -X POST -H "${contentType}" "http://$nacosAddr/nacos/v1/cs/configs?dataId=$(urlencode $1)&group=$group&content=$(urlencode $2)&tenant=$tenant&username=$username&password=$password" >"${tempLog}" 2>/dev/null
+  curl -X PUT -H "${1}" -d "${2}" "http://$3/v1/kv/$4" >"${tempLog}" 2>/dev/null
   if [[ -z $(cat "${tempLog}") ]]; then
     echo " Please check the cluster status. "
     exit 1
   fi
   if [[ $(cat "${tempLog}") =~ "true" ]]; then
-    echo "Set $1=$2 successfully "
+    echo "Set $4=$2 successfully "
   else
-    echo "Set $1=$2 failure "
+    echo "Set $4=$2 failure "
     (( failCount++ ))
-  fi
+ fi
 }
 
 count=0
 for line in $(cat $(dirname "$PWD")/config.txt | sed s/[[:space:]]//g); do
   (( count++ ))
-	key=${line%%=*}
-    value=${line#*=}
-	addConfig "${key}" "${value}"
+  key=${line%%=*}
+  value=${line#*=}
+  addConfig "${contentType}" "${value}" "${consulAddr}" "${key}"
 done
 
 echo "========================================================================="
@@ -106,7 +69,7 @@ echo " Complete initialization parameters,  total-count:$count ,  failure-count:
 echo "========================================================================="
 
 if [[ ${failCount} -eq 0 ]]; then
-	echo " Init nacos config finished, please start seata-server. "
+  echo " Init consul config finished, please start seata-server. "
 else
-	echo " init nacos config fail. "
+  echo " Init consul config fail. "
 fi
